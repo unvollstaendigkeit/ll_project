ec83e086d7bd313e4c7278872beba75dd6ebb334
unvollstaendigkeit <134454134+unvollstaendigkeit@users.noreply.github.com>
2024-07-09 12:04:20 +0200

    simplified controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,48 +11,10 @@ class ArticlesController < ApplicationController
     @comment = Comment.new
   end
 
-  # Display the form for a new article
-  def new
-    @article = Article.new
-  end
-
-  # Create a new article
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      redirect_to @article, notice: 'Article was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # Display the form for editing an article
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  # Update an existing article
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
-      redirect_to @article, notice: 'Article was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # Delete an article
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    redirect_to articles_url, notice: 'Article was successfully destroyed.'
-  end
-
   private
 
-  # Strong parameters for article creation and updating
-  def article_params
-    params.require(:article).permit(:title, :body, :category)
+  # Strong parameters for comment creation
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-  get 'comments/create'
   root "articles#index"
 
-  # Define the articles resource with nested comments
-  resources :articles do
+  resources :articles, only: [:index, :show] do
     resources :comments, only: :create
   end
 
-  # Custom routes for articles
-  get "/articles", to: "articles#index"
-
-  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
-
